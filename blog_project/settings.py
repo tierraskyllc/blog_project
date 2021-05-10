dbe55cbd1053d845819b5daea01da6694c2cfe9d
tierraskyllc <support@tierrasky.com>
2021-05-10 16:10:46 -0400

    Heroku config files and updates

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yr9_3!n)$oubdf+3fgy^mvzy(9n$nahrtqi&a=to_-jz4g6fx7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

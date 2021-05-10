6109f3c7bc4d86854f932b53f4eb47af944634de
tierraskyllc <support@tierrasky.com>
2021-05-10 16:06:13 -0400

    form and user accounts

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,11 @@
 
 # Create your views here.
 
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -28,3 +32,22 @@ class BlogCreateView(CreateView):
     template_name = 'post_new.html'
 
     fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+
+    model = Post
+
+    template_name = 'post_edit.html'
+    
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    
+    model = Post
+
+    template_name = 'post_delete.html'
+
+    success_url = reverse_lazy('home')
+    
